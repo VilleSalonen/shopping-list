dcd812712aa468ba3716a73cca77022b7a3f45dc
Ville Salonen <ville.salonen@iki.fi>
2014-09-11 18:57:25 +0300

    Implemented registration and login

--- client/client.js
+++ client/client.js
@@ -1,5 +1,10 @@
 ShoppingList = new Meteor.Collection("shopping-list");
 
+Handlebars.registerHelper('isLoggedIn', function () {
+    var user = Meteor.user();
+    return !_.isUndefined(user) && !_.isNull(user);
+});
+
 Template.shopping_list.items = function () {
     return ShoppingList.find();
 };
@@ -14,6 +19,25 @@ Template.item_info.isNonDefaultAmount = function (value) {
     return value > 1;
 };
 
+Template.registration.events({
+  'submit #registration-form': function(e, t) {
+    e.preventDefault();
+    
+    // retrieve the input field values
+    var email = t.find('#registration-email').value
+    , password = t.find('#registration-password').value
+    , username = t.find('#registration-name').value;
+
+    Accounts.createUser({ username: username, email: email, password: password }, function(err) {
+      if (err)
+        alert(err);
+    });
+    
+    // Prevent form submission
+    return false; 
+  },
+});
+
 Template.shopping_list.events({
   'click #item_add': function (event, template) {
       event.preventDefault();
@@ -27,4 +51,26 @@ Template.shopping_list.events({
       template.find("input[name=item_amount]").value = "1";
       template.find("input[name=item_name]").focus();
   }
+});
+
+Template.login.events({
+  'submit #login-form': function(e, t) {
+    e.preventDefault();
+    
+    // retrieve the input field values
+    var email = t.find('#login-email').value
+    , password = t.find('#login-password').value;
+
+    Meteor.loginWithPassword(email, password, function(err) {
+      if (err)
+        alert(err);
+    });
+    
+    // Prevent form submission
+    return false; 
+  },
+  
+  'click .logout': function (e, t) {
+    Meteor.logout();
+  },
 });
