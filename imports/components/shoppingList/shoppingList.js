a9c9454d7843417326dbf84f8b78c1de79657dfa
Ville Salonen <ville.salonen@iki.fi>
2016-11-16 21:29:48 +0200

    Items can be marked as bought

--- imports/components/shoppingList/shoppingList.js
+++ imports/components/shoppingList/shoppingList.js
@@ -6,31 +6,42 @@ import { ShoppingList } from "../../api/shoppingItems.js";
 import template from './shoppingList.html';
  
 class ShoppingListCtrl {
-  constructor($scope) {
-    $scope.viewModel(this);
- 
-    this.helpers({
-      shoppingItems() {
-        return ShoppingList.find({});
-      }
-    })
-  }
+    constructor($scope) {
+        $scope.viewModel(this);
+    
+        this.helpers({
+            shoppingItems() {
+                return ShoppingList.find({ bought: false });
+            }
+        })
+    }
 
-  addItem(newItemName, newItemAmount) {
-      ShoppingList.insert({
-          name: newItemName,
-          amount: newItemAmount
-      })
+    addItem(newItemName, newItemAmount) {
+        ShoppingList.insert({
+            name: newItemName,
+            amount: newItemAmount,
+            addedOn: new Date(),
+            bought: false,
+            boughtOn: undefined
+        })
 
-      this.newItemName = "";
-      this.newItemAmount = "";
-  }
+        this.newItemName = "";
+        this.newItemAmount = "";
+    }
+
+    markAsBought(item) {
+        ShoppingList.update(item._id, {
+            $set: {
+                bought: true,
+                boughtOn: new Date()
+            }
+        });
+    }
 }
  
 export default angular.module('shoppingList', [
-  angularMeteor
-])
-  .component('shoppingList', {
+    angularMeteor
+]).component('shoppingList', {
     templateUrl: 'imports/components/shoppingList/shoppingList.html',
     controller: ['$scope', ShoppingListCtrl]
-  });
+});
